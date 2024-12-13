3229398b1b56667fc91e260ee8bf2302a705346a
jun-atmos <whwnsgh20220587@gmail.com>
2024-12-13 22:29:26 +0000

    V2.0.2.1

--- api.py
+++ api.py
@@ -6,9 +6,9 @@ def download_file(tm2):
     cloud_url = f"https://apihub-pub.kma.go.kr/api/typ01/cgi-bin/url/nph-aws2_min_cloud?tm2={tm2}&stn=0&disp=1&help=1&authKey=zbvBntvMSSK7wZ7bzDkiLg"
     vi_url = f"https://apihub-pub.kma.go.kr/api/typ01/cgi-bin/url/nph-aws2_min_vis?tm2={tm2}&stn=0&disp=1&help=1&authKey=zbvBntvMSSK7wZ7bzDkiLg"
 
-    wea_save_file_path = f'./weather_csv/wea_{tm2}.csv'
-    cloud_save_file_path = f'./weather_csv/cloud_{tm2}.csv'
-    vi_save_file_path = f'./weather_csv/vi_{tm2}.csv'
+    wea_save_file_path = f'weather_csv/wea_{tm2}.csv'
+    cloud_save_file_path = f'weather_csv/cloud_{tm2}.csv'
+    vi_save_file_path = f'weather_csv/vi_{tm2}.csv'
 
     # 저장 경로와 파일 URL을 리스트로 설정
     save_paths = [wea_save_file_path, cloud_save_file_path, vi_save_file_path]
